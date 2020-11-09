bcbb7e479deec9409f38f1f61e3ca696d0fbac52
Dominik Basner <your@email.com>
2020-11-09 15:57:57 +0100

    Resolved https://git.isw.uni-stuttgart.de/projekte/industrie/ISW_OPC-UA/2018_VDW_KonI4.0/dashboard/opcua_dashboardclient/-/merge_requests/5#note_16989

--- DashboardClient/DashboardClient.cpp
+++ DashboardClient/DashboardClient.cpp
@@ -60,22 +60,17 @@ namespace Umati {
 			for (auto &pDataSetStorage : m_dataSets) {
 				std::string jsonPayload = getJson(pDataSetStorage);
 				if (!jsonPayload.empty() && jsonPayload != "null") {
-					auto it = m_latestMessages.find(pDataSetStorage->channel);
+					LastMessage_t& lastMessage = m_latestMessages[pDataSetStorage->channel];
+
 					time_t now;
 					time(&now);
-					if (it == m_latestMessages.end()) {
-						m_pPublisher->Publish(pDataSetStorage->channel, jsonPayload);
 
-						LastMessage_t lastMessage;
+					if(jsonPayload != lastMessage.payload || difftime(now, lastMessage.lastSent) > 60) {
+						m_pPublisher->Publish(pDataSetStorage->channel, jsonPayload);
 						lastMessage.payload = jsonPayload;
 						lastMessage.lastSent = now;
-						std::pair<std::string, LastMessage_t> currentMessage(pDataSetStorage->channel, lastMessage);
-						m_latestMessages.insert(currentMessage);
-					} else if (jsonPayload != it->second.payload || difftime(now, it->second.lastSent) > 60) {
-						m_pPublisher->Publish(pDataSetStorage->channel, jsonPayload);
-						it->second.payload = jsonPayload;
-						it->second.lastSent = now;
 					}
+
 				} else {
 					LOG(INFO) << "pdatasetstorage for " << pDataSetStorage->startNodeId.Uri << ";"
 							  << pDataSetStorage->startNodeId.Id << " is empty";
