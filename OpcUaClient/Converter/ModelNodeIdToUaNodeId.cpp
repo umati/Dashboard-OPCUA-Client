6340e09d99a33b577416669ef0cc7cf683fe6196
Marius Dege <marius.dege@basyskom.com>
2021-04-21 17:33:03 +0200

    check for string and integer nodes

--- OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
+++ OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
@@ -10,15 +10,15 @@ namespace Umati {
 														 const std::map<std::string, uint16_t> &uriToID)
 					: ModelToUaConverter(uriToID) {
 
-				//FIXME type? always numeric? better way to get id?
-				//FIXME we need to have to check if its numeric or string!!
-				std::string subs = modelNodeId.Id.substr(2);
-				if (subs.find("5002") != std::string::npos) {
-    			std::cout << "found!" << '\n';
-				}					
+				//FIXME better check for sting and integer
+				if(modelNodeId.Id.substr(0,1) == "i"){
+				std::string subs = modelNodeId.Id.substr(2);					
 				m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),std::stoi(subs));
-			
-			}
+				}else if(modelNodeId.Id.substr(0,1) == "s"){
+				std::string subs = modelNodeId.Id.substr(2);					
+				m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),subs);
+				}
 		}
 	}	
+}
 }
