ca44f4261b13ea4d09048f725066ef23ce234d94
Marius Dege <marius.dege@basyskom.com>
2021-04-26 10:25:26 +0200

    added switch case for Nodeid Types

--- OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
+++ OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
@@ -6,18 +6,43 @@
 namespace Umati {
 	namespace OpcUa {
 		namespace Converter {
+		enum NodeID_Type 
+			{   NODEID_NUMERIC = (int)'i', 
+				NODEID_STRING = (int)'s', 
+				NODEID_GUID = (int)'g',
+				NODEID_BYTESTRING = (int)'b'
+			};
+
+
 			ModelNodeIdToUaNodeId::ModelNodeIdToUaNodeId(const ModelOpcUa::NodeId_t &modelNodeId,
 														 const std::map<std::string, uint16_t> &uriToID)
 					: ModelToUaConverter(uriToID) {
 
-				//FIXME better check for sting and integer
-				if(modelNodeId.Id.substr(0,1) == "i"){
-				std::string subs = modelNodeId.Id.substr(2);					
-				m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),std::stoi(subs));
-				}else if(modelNodeId.Id.substr(0,1) == "s"){
-				std::string subs = modelNodeId.Id.substr(2);					
-				m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),subs);
+				int type = (int)(modelNodeId.Id.substr(0,1)[0]);
+				std::string subs = modelNodeId.Id.substr(2);	
+				switch (type)
+				{
+				case NODEID_NUMERIC:
+				    m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),std::stoi(subs));
+					break;
+
+				case NODEID_STRING:					
+					m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),subs);
+					break;
+
+				case NODEID_GUID:
+					LOG(ERROR) << "Conversion to GUID not implemented.";
+					break;
+
+				case NODEID_BYTESTRING:
+					LOG(ERROR) << "Conversion to ByteString not implemented.";
+					break;
+				
+				default:
+					LOG(ERROR) << "Identifier type not valid";
+					break;
 				}
+
 		}
 	}	
 }
