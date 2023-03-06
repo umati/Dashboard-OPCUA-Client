f7d2b965f0a3ee1e7523b29954b8bf137e13becb
Götz Görisch <g.goerisch@vdw.de>
2023-03-06 14:56:38 +0100

    fix(ci): Builds container image based on debian (#365)

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Copyright 2021 (c) Moritz Walker, ISW University of Stuttagart (for umati and VDW e.V.)
+
+docker build -t opcuaclient -f Dockerfile.debian . --no-cache
+docker container rm -f opcuaclient
+docker run -it --rm -v /home/goetz/projects/Dashboard-OPCUA-Client/configuration.json:/app/configuration.json --name=opcuaclient opcuaclient
