1e57af17b29cbb0edcd2bd47dd1bfa07120a6e65
Sebastian Friedl <friedl.sebastian@web.de>
2023-05-24 12:36:34 +0200

    feat: add guid nodeids (#467)
    * feat: add guid nodeids
    
    * use correct mapping method
    
    * refactor: use parse_nodeid method

--- OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
+++ OpcUaClient/Converter/ModelNodeIdToUaNodeId.cpp
@@ -1,10 +1,11 @@
- /* This Source Code Form is subject to the terms of the Mozilla Public
+/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
- * 
+ *
  * Copyright 2019-2021 (c) Christian von Arnim, ISW University of Stuttgart (for umati and VDW e.V.)
  * Copyright 2020 (c) Dominik Basner, Sotec GmbH (for VDW e.V.)
  * Copyright 2021 (c) Marius Dege, basysKom GmbH
+ * Copyright 2023 (c) Sebastian Friedl, FVA GmbH
  */
 
 #include "ModelNodeIdToUaNodeId.hpp"
@@ -12,51 +13,20 @@
 #include <stdexcept>
 #include <iostream>
 #include <easylogging++.h>
+#include <Open62541Cpp/UA_String.hpp>
 
 namespace Umati {
-	namespace OpcUa {
-		namespace Converter {
-		enum NodeID_Type 
-			{   NODEID_NUMERIC = (int)'i', 
-				NODEID_STRING = (int)'s', 
-				NODEID_GUID = (int)'g',
-				NODEID_BYTESTRING = (int)'b'
-			};
+namespace OpcUa {
+namespace Converter {
 
+enum NodeID_Type { NODEID_NUMERIC = (int)'i', NODEID_STRING = (int)'s', NODEID_GUID = (int)'g', NODEID_BYTESTRING = (int)'b' };
 
-			ModelNodeIdToUaNodeId::ModelNodeIdToUaNodeId(const ModelOpcUa::NodeId_t &modelNodeId,
-														 const std::map<std::string, uint16_t> &uriToID)
-					: ModelToUaConverter(uriToID) {
-
-				int type = (int)(modelNodeId.Id.substr(0,1)[0]);
-				std::string subs = modelNodeId.Id.substr(2);	
-				switch (type)
-				{
-				case NODEID_NUMERIC:
-				    m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),std::stoi(subs));
-					break;
-
-				case NODEID_STRING:					
-					m_nodeId = open62541Cpp::UA_NodeId(getNsIndexFromUri(modelNodeId.Uri),subs);
-					break;
-
-				case NODEID_GUID:
-					LOG(ERROR) << "Conversion to GUID not implemented.";
-					throw "Conversion to GUID not implemented.";
-					break;
-
-				case NODEID_BYTESTRING:
-					LOG(ERROR) << "Conversion to ByteString not implemented.";
-					throw std::invalid_argument("Conversion to ByteString not implemented.");
-					break;
-				
-				default:
-					LOG(ERROR) << "Identifier type not valid";
-					throw "Identifier type not valid";
-					break;
-				}
-
-		}
-	}	
+ModelNodeIdToUaNodeId::ModelNodeIdToUaNodeId(const ModelOpcUa::NodeId_t &modelNodeId, const std::map<std::string, uint16_t> &uriToID)
+  : ModelToUaConverter(uriToID) {
+  open62541Cpp::UA_String tmp(modelNodeId.Id);
+  UA_NodeId_parse(m_nodeId.NodeId, *tmp.String);
+  m_nodeId.NodeId->namespaceIndex = getNsIndexFromUri(modelNodeId.Uri);
 }
-}
+}  // namespace Converter
+}  // namespace OpcUa
+}  // namespace Umati
--- Tests/TestConverter.cpp
+++ Tests/TestConverter.cpp
@@ -5,6 +5,7 @@
  * Copyright 2019-2021 (c) Christian von Arnim, ISW University of Stuttgart (for umati and VDW e.V.)
  * Copyright 2020 (c) Dominik Basner, Sotec GmbH (for VDW e.V.)
  * Copyright 2023 (c) Marc Fischer, ISW University of Stuttgart (for umati and VDW e.V.)
+ * Copyright 2023 (c) Sebastian Friedl, FVA GmbH(for umati and VDW e.V.)
  */
 
 #include <gtest/gtest.h>
@@ -14,15 +15,29 @@
 #include <Converter/ModelQualifiedNameToUaQualifiedName.hpp>
 
 TEST(Converter, NodeId) {
-  ModelOpcUa::NodeId_t nodeId{"MyURI", "s=StringId"};
+  auto nodeids = std::vector<ModelOpcUa::NodeId_t>{{"MyURI", "i=10"}, {"MyURI", "s=StringId"}, {"MyURI", "g=1b9be88d-9249-4cfb-8d08-9a7e32d0d01d"}};
 
   std::map<std::string, uint16_t> uri2Id{{"MyURI", 2}};
   std::map<uint16_t, std::string> id2Uri{{2, "MyURI"}};
 
-  auto uaNodeId = Umati::OpcUa::Converter::ModelNodeIdToUaNodeId(nodeId, uri2Id).getNodeId();
-  auto convNodeId = Umati::OpcUa::Converter::UaNodeIdToModelNodeId(uaNodeId, id2Uri).getNodeId();
+  for (auto nodeId : nodeids) {
+    auto uaNodeId = Umati::OpcUa::Converter::ModelNodeIdToUaNodeId(nodeId, uri2Id).getNodeId();
+    auto convNodeId = Umati::OpcUa::Converter::UaNodeIdToModelNodeId(uaNodeId, id2Uri).getNodeId();
+    ASSERT_EQ(nodeId, convNodeId);
+  }
+}
 
-  EXPECT_EQ(nodeId, convNodeId);
+TEST(Converter, GUID_NodeId) {
+  UA_Guid myGuid = UA_Guid_random();
+  auto nodeid = open62541Cpp::UA_NodeId(2, myGuid);
+
+  std::map<std::string, uint16_t> uri2Id{{"MyURI", 2}};
+  std::map<uint16_t, std::string> id2Uri{{2, "MyURI"}};
+
+  auto modelNodeId = Umati::OpcUa::Converter::UaNodeIdToModelNodeId(nodeid, id2Uri).getNodeId();
+  auto convNodeId = Umati::OpcUa::Converter::ModelNodeIdToUaNodeId(modelNodeId, uri2Id).getNodeId();
+
+  EXPECT_EQ(nodeid, convNodeId);
 }
 
 TEST(Converter, QualifiedName) {
