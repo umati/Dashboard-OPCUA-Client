7afa11eff4238c6517c33e285f8c69fb7ce39f7c
Sebastian Friedl <friedl.sebastian@web.de>
2023-07-19 08:22:33 +0200

    use old fix for isBaseDataVariableType (#559)
    * use old fix for isBaseDataVariableType
    
    * remove log message

--- OpcUaClient/Converter/UaToModelConverter.cpp
+++ OpcUaClient/Converter/UaToModelConverter.cpp
@@ -5,6 +5,8 @@
  * Copyright 2019-2021 (c) Christian von Arnim, ISW University of Stuttgart (for umati and VDW e.V.)
  * Copyright 2020 (c) Dominik Basner, Sotec GmbH (for VDW e.V.)
  * Copyright 2021 (c) Marius Dege, basysKom GmbH
+ * Copyright 2023 (c) Sebastian Friedl, FVA GmbH interop4X
+
  */
 
 #include "UaToModelConverter.hpp"
@@ -22,12 +24,13 @@ namespace Umati {
 
 
 			std::string UaToModelConverter::getUriFromNsIndex(uint16_t nsIndex) {
-				// FIX_BEGIN (FIX_5_PART_1)
-				/*if (nsIndex == 0) {
+				// the opc ua standard allows to remove the nsIndex for the namespace zero
+				// so the converter only use this nsIndex
+				if (nsIndex == 0) {
 					return std::string();
-				}*/
+				}
 				// FIX_END
-				
+
 				auto it = m_idToUri.find(nsIndex);
 				if (it == m_idToUri.end()) {
 					LOG(ERROR) << "Could not find nsIndex: " << nsIndex << std::endl;
